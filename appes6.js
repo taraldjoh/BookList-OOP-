67f27c3ac0985219cc020a4a1d999edf328c81a7
taraldjoh <tarald.johansen@outlook.com>
2019-03-17 19:42:35 +0100

    refactoring

--- appes6.js
+++ appes6.js
@@ -7,63 +7,55 @@ class Book {
 }
 
 class UI {
-  addBookToList() {
-    UI.prototype.addBookToList = function(book) {
-      const list = document.getElementById("book-list");
+  addBookToList(book) {
+    const list = document.getElementById("book-list");
 
-      // Create tr element
-      const row = document.createElement("tr");
+    // Create tr element
+    const row = document.createElement("tr");
 
-      // Insert Cols
-      row.innerHTML = `
+    // Insert Cols
+    row.innerHTML = `
       <td>${book.title}</td>
       <td>${book.author}</td>
       <td>${book.isbn}</td>
       <td><a href="#" class="delete">X<a></td>
       `;
 
-      // Append to list
-      list.appendChild(row);
-    };
+    // Append to list
+    list.appendChild(row);
   }
 
-  showAlert() {
-    UI.prototype.showAlert = function(message, className) {
-      // Create Div
-      const div = document.createElement("div");
-      // Add Classes
-      div.className = `alert ${className}`;
-      //Add Text
-      div.appendChild(document.createTextNode(message));
-      // Get Parent
-      const container = document.querySelector(".container");
+  showAlert(message, className) {
+    // Create Div
+    const div = document.createElement("div");
+    // Add Classes
+    div.className = `alert ${className}`;
+    //Add Text
+    div.appendChild(document.createTextNode(message));
+    // Get Parent
+    const container = document.querySelector(".container");
 
-      const form = document.querySelector("#book-form");
+    const form = document.querySelector("#book-form");
 
-      // Insert Alert
-      container.insertBefore(div, form);
+    // Insert Alert
+    container.insertBefore(div, form);
 
-      // Timeout
-      setTimeout(function() {
-        document.querySelector(".alert").remove();
-      }, 3000);
-    };
+    // Timeout
+    setTimeout(function() {
+      document.querySelector(".alert").remove();
+    }, 3000);
   }
 
-  deleteBook() {
-    UI.prototype.deleteBook = function(target) {
-      if (target.className === "delete") {
-        target.parentElement.parentElement.remove();
-      }
-    };
+  deleteBook(target) {
+    if (target.className === "delete") {
+      target.parentElement.parentElement.remove();
+    }
   }
 
   clearFields() {
-    UI.prototype.clearFields = function() {
-      document.getElementById("title").value = "";
-      document.getElementById("author").value = "";
-      document.getElementById("isbn").value = "";
-    };
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("isbn").value = "";
   }
 }
 
